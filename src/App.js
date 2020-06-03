7de0a1c2e4bbe297e85197d816b93088d3d6f5d8
FengDenny <fengdennyy@gmail.com>
2020-06-03 15:50:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Hero from './pages/Hero/Hero'
+import Navbar from './utility/Navbar/Navbar'
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route to="/" component={Navbar}/>
+      <Route to="/" component={Hero}/>
     </div>
+    </Router>
   );
 }
 
