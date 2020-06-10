b32bf98d0071d88cb9fef47ddefdadbaefe65efa
FengDenny <fengdennyy@gmail.com>
2020-06-10 00:50:26 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Hero from './pages/Hero/Hero'
+// import Hero from './pages/Hero/Hero'
 import Navbar from './utility/Navbar/Navbar'
 function App() {
   return (
     <Router>
     <div className="App">
       <Route to="/" component={Navbar}/>
-      <Route to="/" component={Hero}/>
+
     </div>
     </Router>
   );
